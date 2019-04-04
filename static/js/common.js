471aa0a51aa7b672f7e858057f7d317b71161d4e
s-rainkin <45417223+s-rainkin@users.noreply.github.com>
2019-04-04 18:01:27 +0900

    배너js 메인으로 뺌

--- static/js/common.js
+++ static/js/common.js
@@ -13,25 +13,5 @@ $(function () {
 		$(window).scrollTop(0);
 	});
 
-    /**
-      190111 배너 
-     **/
-    var bnl = $('.bn-left-wrap'), bnr = $('.bn-right-wrap');
-    var bnlH = bnl.height(), bnrH = bnr.height();
-    var bnrt = 60, bnTime = 250;
-    
-    //좌측배너 세로 가운데 정렬
-    bnl.css({'margin-top' : (-bnlH)+'px'});
-    
-    //우측배너 인터렉션
-    bnr.css({'top' : bnrt+'px'});
-    
-    $(window).scroll(function(){
-        if( $(window).scrollTop() < $('#mainTop').offset().top ){
-            bnr.stop().animate({'top' : bnrt+'px', 'margin-top' : 0+'px'}, bnTime);
-        }else {
-            bnr.stop().animate({'top' : 50+'%', 'margin-top' : (-(bnrH/2))+'px'}, bnTime);
-        }
-    });
     
 });
